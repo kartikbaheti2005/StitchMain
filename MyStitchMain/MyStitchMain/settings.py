3c5b55855a96207c1930184bc72fbc9fb49afdac
dev-preshit <preshit.kamble17705@gmail.com>
2026-06-15 21:27:12 +0530

    Add Auths app with user model and phone number field
    - Created Auths app with models, views, and admin registration for AppUser.
    - Added phone number field support using phonenumber_field.
    - Updated settings.py to include Auths app and default region for phone numbers.
    - Added .gitignore to exclude unnecessary files.

--- MyStitchMain/MyStitchMain/settings.py
+++ MyStitchMain/MyStitchMain/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "phonenumber_field",
+    'Auths',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Set the default region code to India
+PHONENUMBER_DEFAULT_REGION = "IN"
