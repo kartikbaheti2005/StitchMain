d541466e27b568ab3f95473fb07e232166fd505b
dev-preshit <preshit.kamble17705@gmail.com>
2026-06-17 10:10:09 +0530

    Fix variable naming conventions in Bill model

--- MyStitchMain/Billing/models.py
+++ MyStitchMain/Billing/models.py
@@ -4,10 +4,10 @@ from Order.models import Order
 
 # Create your models here.
 class Bill(models.Model):
-    Order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
     base_price = models.ForeignKey(ClothPrice, on_delete=models.DO_NOTHING)
     advance = models.DecimalField(max_digits=10, decimal_places=2)
-    Remaning = models.DecimalField(max_digits=10, decimal_places=2)
-    Discount = models.DecimalField(max_digits=10, decimal_places=2)
-    Total = models.DecimalField(max_digits=10, decimal_places=2)
+    remaning = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
